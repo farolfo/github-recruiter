6a9137f0465b26f667255fcc9e5c5f278ad76348
farolfo <francoarolfo@hotmail.com>
2013-10-11 14:49:54 -0300

    added search with angular and tabs

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,37 @@
 'use strict';
 
-/* Controllers */
+var myApp = angular.module('githubRecruiter', []);
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var placeholder = function(tabName) {
+    var placeholder = '';
+    if ( tabName === 'Organization' ) {
+        placeholder = "Enter your organization's name. i.e.: Mulesoft";
+    } else if ( tabName === 'Repository' ) {
+        placeholder = "Enter a repository's name. i.e.: mule";
+    }
+    return placeholder;
+};
 
-  }])
-  .controller('MyCtrl2', [function() {
+myApp.controller('searchController', ['$scope', function($scope){
 
-  }]);
+    $scope.query = '';
+    $scope.searchBy = 'Organization';
+
+    $scope.queryInput = $('#query');
+
+    $scope.search = function() {
+        console.log($scope.query);
+    };
+
+    $scope.switchTab = function(e) {
+        var target = $(e.currentTarget),
+            currentActive = $('.searchContainer').find('li.active');
+
+        $scope.searchBy = target.find('a').text();
+
+        currentActive.removeClass('active');
+        target.addClass('active');
+
+        $scope.queryInput.attr('placeholder', placeholder($scope.searchBy));
+    };
+}]);
